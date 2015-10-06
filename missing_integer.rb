f33b1bae36eaaed5a7aea811248a3664b477dc49
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-10-05 17:22:11 -0700

    MissingInteger

--- missing_integer.rb
+++ missing_integer.rb
@@ -1,12 +1,16 @@
 # Codility Lesson 2: MissingInteger
 # Time Complexity: O(n)
 # Space Complexity: O(n)
+# By pigeonhole principle, at least one of the numbers 1, 2, ..., n+1
+# is not in the array. Let us create a boolean array b of size n+1 to
+# store whether each of these numbers is present.
 
 def solution(a)
-    count = [0, 0]
+    n = a.length
+    count = Array.new(n + 2, 0)
     a.each do |el|
-        next if el < 0
-        count[el].to_i == 0 ? count[el] = 1 : count[el] += 1
+        next if el < 0 || el > n + 1
+        count[el] == 0 ? count[el] = 1 : count[el] += 1
     end
 
     count.each_with_index do |el, index|
