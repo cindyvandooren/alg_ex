e0fce78aaec16f23412a4ffb450ffecdab1238c2
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-10-25 12:12:13 -0700

    Implementation of in place quicksort

--- coursera_course/homework2.rb
+++ coursera_course/homework2.rb
@@ -36,9 +36,10 @@ require "io/console"
 # (We do not require you to submit your code, so feel free to use the programming language of your 
 # choice, just type the numeric answer in the following space.)
 
-# Basic quicksort
+# Basic quicksort:
 # Sorting is not done in place: This is not a memory efficient solution.
-
+# No random pivot.
+# No duplicate entries.
 def qs(arr)
 	return arr if arr.length <= 1 
 
@@ -57,7 +58,64 @@ def qs(arr)
 	return qs(first) + [pivot] + qs(last)
 end
 
+# Sorting is done in place.
+# No random pivot chosen.
+# No duplicate entries.
+# def qsort(arr)
+# 	return arr if arr.length <= 1
 
+# 	pivot = arr.first
+
+# 	# split_i = index between < pivot and > pivot
+# 	split_i = 1
+	
+# 	# Actual partitioning
+# 	arr.each_with_index do |el, index|
+# 		next if index == 0
+# 		if el < pivot
+# 			arr[split_i], arr[index] = el, arr[split_i]
+# 			split_i += 1
+# 		end
+# 	end
+
+# 	arr[0], arr[split_i - 1] = arr[split_i - 1], arr[0]
+
+# 	return qsort(arr[0...split_i - 1]) + [pivot] + qsort(arr[split_i..-1])
+# end
+
+def qsort(to_sort, index_of_pivot = 0, right_index = to_sort.length - 1)
+        old_right_index = right_index   
+        left_index = index_of_pivot 
+         
+        # stop the recursion if nothing to sort
+        return to_sort if left_index >= right_index
+         
+        # partition operation
+        # move both indexes towards the center until they cross over
+        # when left index finds an element greater than pivot and 
+        # right index finds an element smaller than pivot swap them 
+        while left_index < right_index           
+            while to_sort[left_index] <= to_sort[index_of_pivot] and left_index < to_sort.length - 1
+                left_index = left_index + 1
+            end
+             
+            right_index = right_index - 1 until to_sort[right_index] <= to_sort[index_of_pivot] 
+             
+            # swap both elements
+            if left_index < right_index
+                to_sort[left_index], to_sort[right_index] = to_sort[right_index], to_sort[left_index]
+            end
+        end
+         
+        # swap pivot
+        to_sort[index_of_pivot], to_sort[right_index] = to_sort[right_index], to_sort[index_of_pivot]   
+         
+        # recursively sort the sub arrays 
+        sort(to_sort, index_of_pivot, right_index - 1)  
+        sort(to_sort, left_index, old_right_index)
+         
+        return to_sort
+    end
 
 
 # def load_file(file)
